26188062ee11dd3131a9561dd97f07493486b0f6
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2017-02-02 23:26:26 +0300

    ENH: add a crude read_replicas

---
+++ read_replicas.py
@@ -0,0 +1,63 @@
+from __future__ import division, print_function, absolute_import
+import sys
+
+def split_iter(lst, token):
+    """Split a list on the token, yield head and tail."""
+    idx  = lst.index(token)
+    return lst[:idx], lst[idx+1:]
+
+
+def split_list(lst, token=' -----\n'):
+    res = []
+    head = lst
+    try:
+        while True:
+            head, tail = split_iter(head, token)
+            res.append(head)
+            head = tail
+    except ValueError:
+        # we're done, it seems
+        pass
+    return res
+
+## test case, trivial
+#lst = [1, 2, 3, -1, 4, 5, -1, 6, 7, -1, 8]
+#print(split_list(lst, token=-1))
+
+def convert_replica(lst):
+    """Parse the lines for a single replica."""
+    assert len(lst) == 6
+    lst_s = [elem.split('!') for elem in lst]
+
+    r = {}
+    for pair in lst_s:
+        assert len(pair) == 2
+        names = pair[1].strip().split(',')
+        values = [float(_) for _ in pair[0].split()]
+        for name, val in zip(names, values):
+            r[name] = val
+
+    return r
+
+
+
+if __name__ == "__main__":
+    fname = sys.argv[1]
+
+    with open(fname) as f:
+        lines = f.readlines()
+
+    # XXX: skip header for now
+    str_replicas = split_list(lines)
+    str_replicas = str_replicas[1:]
+
+    # convert: replicas will be a list of dicts of {name: value}
+    replicas = [convert_replica(str_replica) for str_replica in str_replicas]
+
+    # compute the Binder cumulant
+    magn2 = [_["av_m2"] for _ in replicas]
+    magn4 = [_["av_m4"] for _ in replicas]
+    Q = [1. - m4/3.0/m2**2 for m2, m4 in zip(magn2, magn4)]
+
+    print("num replicas = ", len(Q))
+    print("Binder Q = ", sum(Q)/len(Q))
